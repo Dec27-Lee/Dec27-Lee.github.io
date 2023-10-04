6b327ccccb729154d197cbe62976762c9a83b6f7
Dec27-Lee <lipengyue31@163.com>
2023-10-04 19:39:35 +0800

    Update site

--- config/js/bbtime.js
+++ config/js/bbtime.js
@@ -2,12 +2,12 @@
  * @Description: 即刻短文首页滚动
  * @LastEditors: 他们叫我跃总 Dec27-Lee
  * @Date: 2023-10-01 13:37:12
- * @LastEditTime: 2023-10-01 13:37:18
+ * @LastEditTime: 2023-10-04 16:59:30
  * @FilePath: \Dec27-Lee.github.iof:\Develop_F\Blog\source\config\js\bbtime.js
  */
 if (document.querySelector('#bber-talk')) {
     var swiper = new Swiper('.swiper-container', {
-        direction: 'vertical', // 垂直切换选项
+        direction: 'vertical',
         loop: true,
         autoplay: {
             delay: 3000,
